b001c657325ebd4bc40547025adfa31a63964035
Steven Woodward <steveanthony999@gmail.com>
2024-05-14 10:10:56 -0700

    Exercises 1.11 and 1.12

--- ch1/section105/main.c
+++ ch1/section105/main.c
@@ -157,7 +157,7 @@
 
 // Exercise 1-10: Write a program to copy it's input to it's output, replacing each tab by \t, each backspace by \b, and each backslash by \\.
 // This makes tabs and backspaces visible in an unambiguous way
-int main() {
+/* int main() {
     int c;
 
     while ((c = getchar()) != EOF) {
@@ -171,16 +171,114 @@ int main() {
             c = '\\';
         }
 
-        if (c == '\b') {
-            putchar('\\');
-            c = 'b';
-        }
-
         putchar(c);
     }
 
     return 0;
 }
+*/
+
+
+// Section 1.5.4 - Word Counting
+
+#define IN  1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+/* count lines, words, and characters in input */
+/* int main() {
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
+*/
+
+// How would you test the word count problem?
+// Basic cases:        Simple sentences.
+//                     Single words.
+//                     Empty inputs.
+// Boundary cases:     Multiple spaces.
+//                     Multiple newlines.
+//                     Tabs.
+// Edge cases:         Input ending with spaces.
+//                     Input ending with newline.
+//                     Input of only spaces, tabs, and newlines.
+// Complex cases:      Mixed punctuation.
+//                     Long input.
+// Special characters: Non-alphabetic
+//                     Special symbols
+// Stress test:        Large input
+
+// What kinds of inputs are most likely to uncover bugs if there are any?
+// State transitions
+
+// Write a program that prints it's input one word per line.
+/* int main() {
+    int c, state;
+    
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+           state = OUT;
+        }
+        else if (state == OUT) {
+            state = IN;
+            putchar('\n');
+        }
+        putchar(c);
+    }
+
+    return 0;
+}
+*/
+
+// Variation
+int main() {
+    int c, state;
+    
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t') {
+            if (state == IN) {
+                putchar('\\');
+                putchar('n');
+                putchar('\n');
+                state = OUT;
+            }
+        } else {
+            if (state == OUT) {
+                state = IN;
+            }
+            putchar(c);
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
